208565471a581aab380bc0f1b79c033a66a73ec3
xiaoyv404 <xiaoyv_404@outlook.com>
2022-04-02 21:02:27 +0800

    删除 PluginMain.kt 中对 SomeThing 模块的引用

--- src/main/kotlin/com/xiaoyv404/mirai/PluginMain.kt
+++ src/main/kotlin/com/xiaoyv404/mirai/PluginMain.kt
@@ -1,6 +1,5 @@
 package com.xiaoyv404.mirai
 
-import com.xiaoyv404.mirai.app.someThinkEntrance
 import com.xiaoyv404.mirai.core.App
 import com.xiaoyv404.mirai.core.NfApp
 import com.xiaoyv404.mirai.core.NfApplicationManager
@@ -32,8 +31,6 @@ object PluginMain : KotlinPlugin(
     override fun onEnable() {
         PluginConfig.reload()
         connect()
-
-        someThinkEntrance()
 
         val f = Reflections("com.xiaoyv404.mirai.app")
         val set: Set<Class<*>> = f.getTypesAnnotatedWith(App::class.java)
