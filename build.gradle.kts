49e093b150403cb18d3cf425a8a2e5c8a1e87dab
xiaoyv404 <xiaoyv_404@outlook.com>
2022-04-05 00:07:20 +0800

    删除不必要的包

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,7 +45,6 @@ dependencies {
     implementation("org.ktorm:ktorm-core:$ktorm")
     implementation("com.zaxxer:HikariCP:5.0.1")
 
-    implementation("br.com.devsrsouza:redissed:1.1.0")
     implementation("io.lettuce:lettuce-core:5.3.7.RELEASE")
 
     implementation("com.alibaba:fastjson:1.2.80")
