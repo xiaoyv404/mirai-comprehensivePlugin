9be8e378ff36acb605f3ea94dc8d4734f7133cbf
xiaoyv404 <xiaoyv_404@outlook.com>
2022-04-22 21:14:20 +0800

    [重构]消息记录器模块

--- src/main/kotlin/com/xiaoyv404/mirai/app/history/process.kt
+++ src/main/kotlin/com/xiaoyv404/mirai/app/history/process.kt
@@ -2,13 +2,15 @@ package com.xiaoyv404.mirai.app.history
 
 import com.xiaoyv404.mirai.PluginMain
 import com.xiaoyv404.mirai.core.App
-import com.xiaoyv404.mirai.core.NfApp
+import com.xiaoyv404.mirai.core.NfAppMessageHandler
+import com.xiaoyv404.mirai.core.gid
+import com.xiaoyv404.mirai.core.uid
 import com.xiaoyv404.mirai.databace.dao.HistoryRecord
 import com.xiaoyv404.mirai.databace.dao.save
 import com.xiaoyv404.mirai.tool.FileUtils
 import kotlinx.serialization.SerializationException
-import net.mamoe.mirai.event.GlobalEventChannel
-import net.mamoe.mirai.event.subscribeGroupMessages
+import net.mamoe.mirai.contact.Group
+import net.mamoe.mirai.event.events.MessageEvent
 import net.mamoe.mirai.message.data.*
 import net.mamoe.mirai.message.data.MessageChain.Companion.serializeToJsonString
 import java.time.Instant
@@ -17,61 +19,58 @@ import java.time.ZoneId
 import java.time.ZoneOffset
 
 @App
-class History : NfApp(){
+class History : NfAppMessageHandler() {
     override fun getAppName() = "History"
     override fun getVersion() = "1.0.0"
     override fun getAppDescription() = "消息记录器"
-    override fun init() {
-        GlobalEventChannel.subscribeGroupMessages {
-            always {
-                val msg = when (message[1]) {
-                    is PlainText      -> {
-                        message.content
-                    }
-                    is Image          -> {
-                        message.content
-                    }
-                    is At             -> {
-                        message.content
-                    }
-                    is AtAll          -> {
-                            message.content
-                        }
-                        is Face       -> {
-                            message.content
-                        }
-                        is MarketFace -> {
-                            message.content
-                        }
-                        else          -> {
-                            return@always
-                        }
-                    }
-                    FileUtils.saveFileFromString(
-                        msg + "\n",
-                        PluginMain.resolveDataFile("history/${group.id}.txt")
-                    )
-
-                try {
-                    HistoryRecord {
-                        sendTime = LocalDateTime.ofInstant(
-                            Instant.ofEpochSecond(message.time.toLong() + 10800), ZoneId.ofOffset(
-                                "UTC",
-                                ZoneOffset.of("+18")
-                            )
-                        )
-                        eventType = "msg"
-                        groupId = group.id
-                        groupName = group.name
-                        senderId = sender.id
-                        senderName = sender.nameCard
-                        msgId = message.ids[0].toLong()
-                        content = message.serializeToJsonString()
-                    }.save()
-                } catch (_: SerializationException) {
-                }
+    override suspend fun handleMessage(msg: MessageEvent) {
+        val message = msg.message
+        val group = msg.subject as Group
+        when (message[1]) {
+            is PlainText  -> {
+                message.content
             }
+            is Image      -> {
+                message.content
+            }
+            is At         -> {
+                message.content
+            }
+            is AtAll      -> {
+                message.content
+            }
+            is Face       -> {
+                message.content
+            }
+            is MarketFace -> {
+                message.content
+            }
+            else          -> {
+                return
+            }
+        }.let {
+            FileUtils.saveFileFromString(
+                it + "\n",
+                PluginMain.resolveDataFile("history/${msg.gid()}.txt")
+            )
         }
-
+        try {
+            HistoryRecord {
+                sendTime = LocalDateTime.ofInstant(
+                    Instant.ofEpochSecond(message.time.toLong() + 10800), ZoneId.ofOffset(
+                        "UTC",
+                        ZoneOffset.of("+18")
+                    )
+                )
+                eventType = "msg"
+                groupId = msg.gid()
+                groupName = group.name
+                senderId = msg.uid()
+                senderName = msg.senderName
+                msgId = message.ids[0].toLong()
+                content = message.serializeToJsonString()
+            }.save()
+        } catch (_: SerializationException) {
+        }
     }
 }
