0c41c3ad96a99f85a93cc5ab03e8a43dd6384eab
xiaoyv404 <xiaoyv_404@outlook.com>
2022-08-27 11:46:36 +0800

    [依赖]更新包版本

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
 
-    id("net.mamoe.mirai-console") version "2.12.0"
+    id("net.mamoe.mirai-console") version "2.12.2"
 }
 
 group = "com.xiaoyv404"
