0e4a12802581b3c2a67121c975337688e7a97c45
xiaoyv404 <xiaoyv_404@outlook.com>
2022-09-17 18:26:00 +0800

    [重构]返回结构统一测试

--- src/main/kotlin/com/xiaoyv404/mirai/app/webAPI/controller/NfResult.kt
+++ src/main/kotlin/com/xiaoyv404/mirai/app/webAPI/controller/NfResult.kt
@@ -6,33 +6,31 @@ enum class ResultEnum(override val code: Int, override val msg: String) : IResul
     COMMON_FAILED(2003, "接口调用失败"),
     FORBIDDEN(2004, "没有权限访问资源");
 }
-
-public class NfResult<T>(code: Int, msg: String, data: T) {
-
+@kotlinx.serialization.Serializable
+data class NfResult<T>(val code: Int, val msg: String, val data: T) {
     companion object{
         fun <T> success(data: T): NfResult<T> {
             return NfResult(ResultEnum.SUCCESS.code, ResultEnum.SUCCESS.msg, data)
         }
 
-        fun <T> success(message: String, data: T): NfResult<T> {
-            return NfResult(ResultEnum.SUCCESS.code, message, data)
+        fun <T> success(msg: String, data: T): NfResult<T> {
+            return NfResult(ResultEnum.SUCCESS.code, msg, data)
         }
 
         fun failed(): NfResult<Any?> {
             return NfResult(ResultEnum.COMMON_FAILED.code, ResultEnum.COMMON_FAILED.msg, null)
         }
 
-        fun failed(message: String): NfResult<Any?> {
-            return NfResult(ResultEnum.COMMON_FAILED.code, message, null)
+        fun failed(msg: String): NfResult<Any?> {
+            return NfResult(ResultEnum.COMMON_FAILED.code, msg, null)
         }
 
-        fun failed(errorResult: IResult): NfResult<Any?> {
+        fun failed(errorResult: IResult): NfResult<Nothing?> {
             return NfResult(errorResult.code, errorResult.msg, null)
         }
 
         fun <T> instance(code: Int, message: String, data: T): NfResult<T> {
             return NfResult(code, message, data)
         }
-
     }
 }
