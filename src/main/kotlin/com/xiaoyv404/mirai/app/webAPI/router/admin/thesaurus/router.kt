c6633fd6065a66df80c6c2712c518d3b3b4aa50c
xiaoyv404 <xiaoyv_404@outlook.com>
2022-11-27 13:24:34 +0800

    [webApi]fix thesaurusRoute is wrong

--- src/main/kotlin/com/xiaoyv404/mirai/app/webAPI/router/admin/thesaurus/router.kt
+++ src/main/kotlin/com/xiaoyv404/mirai/app/webAPI/router/admin/thesaurus/router.kt
@@ -3,7 +3,7 @@ package com.xiaoyv404.mirai.app.webAPI.router.admin.thesaurus
 import io.ktor.server.routing.*
 
 fun Route.thesaurusRouter(){
-    route("thesaurus"){
+    route("/thesaurus"){
         finByQuestion()
     }
 }
