94b5b95754adf34f900399c10c5afbc601126602
xiaoyv404 <xiaoyv_404@outlook.com>
2023-01-20 15:03:55 +0800

    [webApi]Add some MinecraftServerApi

---
+++ src/main/kotlin/com/xiaoyv404/mirai/app/webAPI/router/mincreaftServer/player.kt
@@ -0,0 +1,26 @@
+package com.xiaoyv404.mirai.app.webAPI.router.mincreaftServer
+
+import com.xiaoyv404.mirai.app.webAPI.*
+import com.xiaoyv404.mirai.app.webAPI.controller.*
+import com.xiaoyv404.mirai.databace.dao.mincraftServer.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Route.player() {
+    get("/players/{id}") {
+        val id = call.parameters["id"] ?: error(WebApi.requestError)
+        val data = MinecraftServerPlayer {
+            this.id = id
+        }.findById() ?: error(WebApi.requestError)
+        call.respond(NfResult.success(data))
+    }
+
+    get("/players/search") {
+        val name = call.request.queryParameters["name"]?: error(WebApi.requestError)
+        val data = MinecraftServerPlayer{
+            this.name = name
+        }.findByName()?: error(WebApi.requestError)
+        call.respond(NfResult.success(data))
+    }
+}
---
+++ src/main/kotlin/com/xiaoyv404/mirai/app/webAPI/router/mincreaftServer/server.kt
@@ -0,0 +1,40 @@
+package com.xiaoyv404.mirai.app.webAPI.router.mincreaftServer
+
+import com.xiaoyv404.mirai.app.webAPI.*
+import com.xiaoyv404.mirai.app.webAPI.controller.*
+import com.xiaoyv404.mirai.databace.dao.*
+import com.xiaoyv404.mirai.databace.dao.mincraftServer.*
+import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Route.server() {
+    get("/server/{n}") {
+        val n = call.parameters["n"] ?: error(WebApi.requestError)
+        if (n == "all") {
+            val map = getAll()
+            call.respond(NfResult.success(map))
+            return@get
+        }
+        val serverID = n.toIntOrNull() ?: error(WebApi.requestError)
+        val data = MinecraftServer {
+            this.id = serverID
+        }.findById()
+        call.respond(NfResult.success(data))
+    }
+    patch("/server/{n}") {
+        val principal = call.principal<UserIdPrincipal>() ?: error(WebApi.noPrincipal)
+        val n = call.parameters["n"]?.toIntOrNull() ?: error(WebApi.requestError)
+        principal.name.permissionRequiredAdmin()
+
+        val data = call.receive<MinecraftServer>()
+        data.id = n
+        val changeLine = data.update()
+        if (changeLine == 0)
+            WebApi.requestError
+        else
+            call.respond(NfResult.success(changeLine))
+    }
+}
