529235e31722e2e85b58367b84e299cd0bedf3be
xiaoyv404 <xiaoyv_404@outlook.com>
2023-01-29 11:26:02 +0800

    [自动通过加群系统]添加测试代码

--- src/main/kotlin/com/xiaoyv404/mirai/app/groupHelper/MemberJoinRequest.kt
+++ src/main/kotlin/com/xiaoyv404/mirai/app/groupHelper/MemberJoinRequest.kt
@@ -8,10 +8,13 @@ class MemberJoinRequest : NfAppMemberJoinRequestHandler() {
     override fun getAppName() = "群申请自动审批功能"
     override fun getVersion() = "1.0.0"
     override suspend fun handleMessage(event: MemberJoinRequestEvent) {
+        println("1111")
         if (event.groupId != 705664551L)
             return
+        println(222222)
 
         val requester = event.bot.getStranger(event.fromId)?: return
+        println(11111111)
         if(requester.queryProfile().qLevel >= 32) {
             event.accept()
             event.bot.getFriend(2083664136)!!.sendMessage("通过 ${event.fromNick}的群申请")
