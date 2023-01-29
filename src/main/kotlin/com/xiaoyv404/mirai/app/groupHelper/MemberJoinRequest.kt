048c4fd807fa486d31daa126a60ab6fc779bba14
xiaoyv404 <xiaoyv_404@outlook.com>
2023-01-29 11:33:32 +0800

    [自动通过加群系统]修复无法正常通过的问题

--- src/main/kotlin/com/xiaoyv404/mirai/app/groupHelper/MemberJoinRequest.kt
+++ src/main/kotlin/com/xiaoyv404/mirai/app/groupHelper/MemberJoinRequest.kt
@@ -1,6 +1,7 @@
 package com.xiaoyv404.mirai.app.groupHelper
 
 import com.xiaoyv404.mirai.core.*
+import net.mamoe.mirai.*
 import net.mamoe.mirai.event.events.*
 
 @App
@@ -8,17 +9,14 @@ class MemberJoinRequest : NfAppMemberJoinRequestHandler() {
     override fun getAppName() = "群申请自动审批功能"
     override fun getVersion() = "1.0.0"
     override suspend fun handleMessage(event: MemberJoinRequestEvent) {
-        println("1111")
         if (event.groupId != 705664551L)
             return
-        println(222222)
 
-        val requester = event.bot.getStranger(event.fromId)?: return
-        println(11111111)
-        if(requester.queryProfile().qLevel >= 32) {
+        val profile = Mirai.queryProfile(event.bot, event.fromId)
+        if(profile.qLevel >= 32) {
             event.accept()
             event.bot.getFriend(2083664136)!!.sendMessage("通过 ${event.fromNick}的群申请")
         }else
-            event.bot.getFriend(2083664136)!!.sendMessage(requester.queryProfile().qLevel.toString())
+            event.bot.getFriend(2083664136)!!.sendMessage(profile.qLevel.toString())
     }
 }
