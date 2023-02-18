cd9fcf5f7d49f5c6e4cdcefcf1aff4fdaa24dbc6
xiaoyv404 <xiaoyv_404@outlook.com>
2023-02-18 15:30:19 +0800

    [修改]catch UnrecognizedOptionException

--- src/main/kotlin/com/xiaoyv404/mirai/app/fsh/Fsh.kt
+++ src/main/kotlin/com/xiaoyv404/mirai/app/fsh/Fsh.kt
@@ -1,21 +1,15 @@
 package com.xiaoyv404.mirai.app.fsh
 
-import com.xiaoyv404.mirai.NfPluginData
-import com.xiaoyv404.mirai.app.thesaurus.cMsgToMiraiMsg
-import com.xiaoyv404.mirai.app.thesaurus.parseMsg
-import com.xiaoyv404.mirai.core.App
+import com.xiaoyv404.mirai.*
+import com.xiaoyv404.mirai.app.thesaurus.*
+import com.xiaoyv404.mirai.core.*
 import com.xiaoyv404.mirai.core.MessageProcessor.reply
-import com.xiaoyv404.mirai.core.NfApp
-import com.xiaoyv404.mirai.core.NfAppMessageHandler
-import com.xiaoyv404.mirai.core.NfApplicationManager
-import com.xiaoyv404.mirai.databace.dao.Thesauru
-import com.xiaoyv404.mirai.databace.dao.authorityIdentification
-import com.xiaoyv404.mirai.databace.dao.findByQuestion
-import com.xiaoyv404.mirai.databace.dao.isBot
-import net.mamoe.mirai.event.events.MessageEvent
-import net.mamoe.mirai.message.code.MiraiCode
-import net.mamoe.mirai.message.data.OnlineMessageSource
-import java.util.regex.Pattern
+import com.xiaoyv404.mirai.databace.dao.*
+import net.mamoe.mirai.event.events.*
+import net.mamoe.mirai.message.code.*
+import net.mamoe.mirai.message.data.*
+import org.apache.commons.cli.*
+import java.util.regex.*
 
 
 @App
@@ -33,15 +27,17 @@ class Fsh : NfAppMessageHandler() {
         val uid: Long
         val gid: Long
         when (incoming) {
-            is OnlineMessageSource.Incoming.FromGroup  -> {
+            is OnlineMessageSource.Incoming.FromGroup -> {
                 gid = incoming.group.id
                 uid = incoming.sender.id
             }
+
             is OnlineMessageSource.Incoming.FromFriend -> {
                 gid = 0
                 uid = incoming.sender.id
             }
-            else                                       -> {
+
+            else -> {
                 return
             }
         }
@@ -80,6 +76,8 @@ class Fsh : NfAppMessageHandler() {
                         // 调用成功进行限制计次
                         fshApp.submitCallLimiter(uid, gid)
                     }
+                } catch (_: UnrecognizedOptionException) {
+                    msg.reply("未知参数")
                 } catch (e: Exception) {
                     if (debug) {
                         msg.reply("处理${argsList[0]}命令发生异常\n$e", quote = true)
