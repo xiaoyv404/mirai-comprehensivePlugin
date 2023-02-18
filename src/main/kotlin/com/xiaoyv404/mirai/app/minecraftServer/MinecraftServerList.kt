814a3562db3b71eb39896c978c72213e2ec17242
xiaoyv404 <xiaoyv_404@outlook.com>
2023-02-18 15:24:09 +0800

    [新功能]服务器列表提示

---
+++ src/main/kotlin/com/xiaoyv404/mirai/app/minecraftServer/MinecraftServerList.kt
@@ -0,0 +1,28 @@
+package com.xiaoyv404.mirai.app.minecraftServer
+
+import com.xiaoyv404.mirai.app.fsh.*
+import com.xiaoyv404.mirai.core.*
+import com.xiaoyv404.mirai.core.MessageProcessor.reply
+import com.xiaoyv404.mirai.databace.dao.mincraftServer.*
+import net.mamoe.mirai.contact.Contact.Companion.uploadImage
+import net.mamoe.mirai.event.events.*
+import net.mamoe.mirai.message.data.*
+
+class MinecraftServerList : NfApp(), IFshApp {
+    override fun getAppName() = "MinecraftServerList"
+    override fun getVersion() = "1.0.2"
+    override fun getAppDescription() = "我的世界服务器状态监测-列表"
+    override fun getCommands() =
+        arrayOf(
+            "-土豆列表",
+            "-服务器列表"
+        )
+
+    override suspend fun executeRsh(args: Array<String>, msg: MessageEvent): Boolean {
+        val list = MinecraftServer().toList()
+        val img = MinecraftDataImgGenerator().drawList(list)
+        msg.reply(msg.subject.uploadImage(img).toMessageChain())
+        return true
+    }
+
+}
--- src/main/kotlin/com/xiaoyv404/mirai/databace/dao/mincraftServer/MinecraftServer.dao.kt
+++ src/main/kotlin/com/xiaoyv404/mirai/databace/dao/mincraftServer/MinecraftServer.dao.kt
@@ -14,6 +14,8 @@ interface MinecraftServer : Entity<MinecraftServer> {
     val port: Int
     var status: Int
     val name: String
+    var playerNum: Int
+    var playerMaxNum: Int
 }
 
 private val Database.minecraftServers get() = this.sequenceOf(MinecraftServers)
@@ -34,6 +36,10 @@ fun String.findByName(): MinecraftServer?{
     return db.minecraftServers.find { MinecraftServers.name eq this }
 }
 
+fun MinecraftServer.toList(): List<MinecraftServer>{
+    return db.minecraftServers.toList()
+}
+
 
 object MinecraftServers : Table<MinecraftServer>("MinecraftServer") {
     val id = int("id").primaryKey().bindTo { it.id }
@@ -41,4 +47,6 @@ object MinecraftServers : Table<MinecraftServer>("MinecraftServer") {
     val port = int("port").bindTo { it.port }
     val status = int("status").bindTo { it.status }
     val name = varchar("name").bindTo { it.name }
+    val playerNum  = int("playerNum").bindTo { it.playerNum }
+    val playerMaxNum = int("playerMaxNum").bindTo { it.playerMaxNum }
 }
