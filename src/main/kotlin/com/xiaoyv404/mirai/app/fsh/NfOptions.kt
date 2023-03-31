8c53fd320903fa9094977e5d32fed50fcb0e7edc
xiaoyv404 <xiaoyv_404@outlook.com>
2023-04-01 00:57:58 +0800

    修复没有正常添加指令的问题

--- src/main/kotlin/com/xiaoyv404/mirai/app/fsh/NfOptions.kt
+++ src/main/kotlin/com/xiaoyv404/mirai/app/fsh/NfOptions.kt
@@ -2,10 +2,8 @@ package com.xiaoyv404.mirai.app.fsh
 
 import org.apache.commons.cli.*
 
-class NfOptions : Options(){
+class NfOptions : Options() {
     init {
-        Options().apply {
-            addOption("h", "help", false, "查看使用说明")
-        }
+        addOption("h", "help", false, "查看使用说明")
     }
 }
