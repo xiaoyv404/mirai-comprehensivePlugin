bbd0bfd18e9e39caf37adfa54d0bdec96a3a2b81
xiaoyv404 <xiaoyv_404@outlook.com>
2023-07-05 20:23:45 +0800

    Fix command words can't trigger problem

--- src/main/kotlin/com/xiaoyv404/mirai/app/UserAlert.kt
+++ src/main/kotlin/com/xiaoyv404/mirai/app/UserAlert.kt
@@ -1,9 +1,8 @@
 package com.xiaoyv404.mirai.app
 
-import com.xiaoyv404.mirai.app.fsh.IFshApp
 import com.xiaoyv404.mirai.core.App
 import com.xiaoyv404.mirai.core.MessageProcessor.reply
-import com.xiaoyv404.mirai.core.NfApp
+import com.xiaoyv404.mirai.core.NfAppMessageHandler
 import com.xiaoyv404.mirai.dao.findById
 import com.xiaoyv404.mirai.dao.isNotAdmin
 import com.xiaoyv404.mirai.dao.save
@@ -16,22 +15,23 @@ import net.mamoe.mirai.message.data.At
 import net.mamoe.mirai.message.data.orNull
 
 @App
-class UserAlert : NfApp(), IFshApp {
+class UserAlert : NfAppMessageHandler() {
     override fun getAppName() = "UserAlert"
     override fun getVersion() = "1.0.0"
     override fun getAppDescription() = "用户警告相关"
-    override fun getCommands() = arrayOf("警告")
-    override suspend fun executeRsh(args: Array<String>, msg: MessageEvent): Boolean {
+    override suspend fun handleMessage(msg: MessageEvent) {
+        if (msg.message.contentToString().startsWith("警告"))
+            return
         if (msg.subject !is Group)
-            return false
+            return
         val sender = msg.sender as Member
         if (msg.isNotAdmin() && !sender.permission.isOperator())
-            return false
+            return
 
         val at: At? by msg.message.orNull()
 
         if (at == null)
-            return false
+            return
 
         val user = User {
             this.id = at!!.target
@@ -44,6 +44,6 @@ class UserAlert : NfApp(), IFshApp {
 
         msg.reply("已警告，本次为第${user.warningTimes}次警告")
 
-        return true
+        return
     }
 }
