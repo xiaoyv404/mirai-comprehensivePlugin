c936175fb4e908e5a8828cd3918ab4d574290a86
小宇 <63290381+xiaoyv404@users.noreply.github.com>
2023-07-06 22:21:49 +0800

    Main (#73)
    * Develop (#68)
    
    * Feature user alert module (#66)
    
    * Add User data table warningTimes Field
    
    * Add user warn feature
    
    * Fix Logical Judgment
    
    * Fix command words can't trigger problem
    
    * Fix logic
    
    * Feature/test (#72)
    
    * remove: test folder from .gitignore
    
    * add: testing api
    upgrade: mirai to 2.15.0-rc
    
    * optimize: change method of obtaining logger
    
    * add: testing base
    
    * add: testUserAlertWarn
    
    * ci: use JDK 17
    
    * ci: Gradle CI use JDK 17

---
+++ src/test/kotlin/com/xiaoyv404/mirai/TestBase.kt
@@ -0,0 +1,34 @@
+package com.xiaoyv404.mirai
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.fail
+import kotlin.reflect.jvm.jvmName
+
+internal open class TestBase {
+    internal inline fun <reified T> assertIsInstance(value: Any?, block: T.() -> Unit = {}) {
+        if (value !is T) {
+            fail { "Actual value $value (${value?.javaClass}) is not instanceof ${T::class.jvmName}" }
+        }
+        block(value)
+    }
+    internal fun execSqlScript(filename: String) {
+        com.xiaoyv404.mirai.databace.Database.db.useConnection { conn ->
+            conn.createStatement().use { statement ->
+                javaClass.classLoader
+                    ?.getResourceAsStream(filename)
+                    ?.bufferedReader()
+                    ?.use { reader ->
+                        for (sql in reader.readText().split(';')) {
+                            if (sql.any { it.isLetterOrDigit() }) {
+                                statement.executeUpdate(sql)
+                            }
+                        }
+                    }
+            }
+        }
+    }
+    internal fun runTest(action: suspend CoroutineScope.() -> Unit) {
+        runBlocking(block = action)
+    }
+}
