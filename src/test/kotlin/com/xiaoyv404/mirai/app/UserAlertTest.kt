873d0a02605b70d32d5ab51c7d1e54124963c7b6
xiaoyv404 <xiaoyv_404@outlook.com>
2023-07-07 08:44:38 +0800

    add: more testUserAlertWarn case

--- src/test/kotlin/com/xiaoyv404/mirai/app/UserAlertTest.kt
+++ src/test/kotlin/com/xiaoyv404/mirai/app/UserAlertTest.kt
@@ -1,16 +1,11 @@
 package com.xiaoyv404.mirai.app
 
 import com.xiaoyv404.mirai.BaseTest
-import kotlinx.coroutines.flow.toList
 import net.mamoe.mirai.LowLevelApi
 import net.mamoe.mirai.contact.MemberPermission
-import net.mamoe.mirai.event.Event
-import net.mamoe.mirai.event.GlobalEventChannel
-import net.mamoe.mirai.event.events.GroupMessageEvent
+import net.mamoe.mirai.event.events.GroupMessagePostSendEvent
+import net.mamoe.mirai.event.events.MessageEvent
 import net.mamoe.mirai.message.data.At
-import net.mamoe.mirai.message.data.OnlineMessageSource
-import net.mamoe.mirai.message.data.buildMessageChain
-import net.mamoe.mirai.message.data.source
 import net.mamoe.mirai.mock.utils.simpleMemberInfo
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
@@ -19,22 +14,30 @@ internal class UserAlertTest : BaseTest() {
     @OptIn(LowLevelApi::class)
     @Test
     internal fun testUserAlertWarn() = runTest {
-        val mockGroup = bot.addGroup(1147939635, "MC幻想乡-旧约酒馆")
-        val mockMember = mockGroup
-            .addMember(simpleMemberInfo(5971, "test", permission = MemberPermission.OWNER))
-
-        mockMember.says(buildMessageChain {
-            +"警告"
-            +At(1111)
-        })
-        GlobalEventChannel.subscribeOnce<Event> {
-            assertIsInstance<GroupMessageEvent>(this) {
-                UserAlert().handleMessage(this)
-                assertIsInstance<OnlineMessageSource.Incoming.FromGroup>(message.source)
+        runAndReceiveEventBroadcast {
+            val mockMember = bot.addGroup(1147939635, "MC幻想乡-旧约酒馆")
+                .addMember(simpleMemberInfo(5971, "test", permission = MemberPermission.OWNER))
+            mockMember.says {
+                +"警告"
+                +At(1111)
             }
-        }
-        mockGroup.roamingMessages.getAllMessages().toList().let { msg ->
-            assertEquals("警告@1111", msg[0].contentToString())
+            mockMember.says {
+                +"警告一次"
+                +At(1111)
+                +At(1008611)
+                +At(464)
+            }
+        }.filterIsInstance<MessageEvent>().runNfMessageEventApp {
+            UserAlert().handleMessage(this)
+        }.filterIsInstance<GroupMessagePostSendEvent>().let { msg ->
+            assertEquals("已警告@1111，本次为第1次警告", msg[0].message.contentToString())
+            assertEquals(
+                """
+                已警告@1111，本次为第2次警告
+                已警告@1008611，本次为第1次警告
+                已警告@464，本次为第1次警告
+            """.trimIndent(), msg[1].message.contentToString()
+            )
         }
     }
 }
