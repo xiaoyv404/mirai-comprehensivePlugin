c19aa9903c1456df72b2c5f9560db850f8994c64
xiaoyv404 <xiaoyv_404@outlook.com>
2023-07-19 11:55:15 +0800

    add: MinecraftServerPlayerQQMapping table

---
+++ src/main/kotlin/com/xiaoyv404/mirai/model/mincraftServer/MinecraftServerPlayerQQMapping.kt
@@ -0,0 +1,18 @@
+package com.xiaoyv404.mirai.model.mincraftServer
+
+import org.ktorm.entity.Entity
+import org.ktorm.schema.Table
+import org.ktorm.schema.long
+import org.ktorm.schema.varchar
+
+interface MinecraftServerPlayerQQMapping:Entity<MinecraftServerPlayerQQMapping> {
+    companion object : Entity.Factory<MinecraftServerPlayerQQMapping>()
+
+    var qq: Long
+    var playerName: String
+}
+
+object MinecraftServerPlayerQQMappings : Table<MinecraftServerPlayerQQMapping>("MinecraftServerPlayer_qq_mapping") {
+    val qq = long("qq").primaryKey().bindTo { it.qq }
+    val playerName = varchar("playerName").primaryKey().bindTo { it.playerName }
+}
