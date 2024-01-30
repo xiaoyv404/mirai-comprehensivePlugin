0bb6f9a1090bdb15d66db46b5ade1fd5db1b53d7
xiaoyv404 <xiaoyv_404@outlook.com>
2024-01-30 13:47:19 +0800

    rm test code

--- src/test/kotlin/com/xiaoyv404/mirai/app/AdminToolsTest.kt
+++ src/test/kotlin/com/xiaoyv404/mirai/app/AdminToolsTest.kt
@@ -2,20 +2,14 @@ package com.xiaoyv404.mirai.app
 
 import com.xiaoyv404.mirai.BaseTest
 import com.xiaoyv404.mirai.PluginData
-import com.xiaoyv404.mirai.core.NfClock
 import net.mamoe.mirai.event.events.FriendMessagePostSendEvent
 import org.junit.jupiter.api.Test
-import java.time.Clock
-import java.time.Instant
-import java.time.ZoneId
 import kotlin.test.assertEquals
 
 internal class AdminToolsTest : BaseTest() {
 
     @Test
     internal fun testCommandDebug() = runTest {
-        val clock = Clock.fixed(Instant.parse("2006-04-16T06:59:39.00Z"), ZoneId.of("UTC"))
-        NfClock.mockTime(clock)
         assertEquals(PluginData.deBug, false)
         runAndReceiveEventBroadcast {
             val mockFriend = bot.addFriend(123, "test")
