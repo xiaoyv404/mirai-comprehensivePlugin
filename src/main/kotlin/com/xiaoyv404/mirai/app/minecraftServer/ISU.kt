b1408ef78e19737fa79c21bcfdc0697094465b6e
xiaoyv_404 <xiaoyv_404@outlook.com>
2025-11-27 14:48:30 +0800

    给admin添加isu more bypass

--- src/main/kotlin/com/xiaoyv404/mirai/app/minecraftServer/ISU.kt
+++ src/main/kotlin/com/xiaoyv404/mirai/app/minecraftServer/ISU.kt
@@ -79,7 +79,7 @@ class ISU : NfApp(), IFshApp {
         }
 
         if (more && (MinecraftServerPlayerQQMapping { qq = msg.uid() }.getPermissionByQQ()
-                ?: Permissions.Default) < Permissions.OP
+                ?: Permissions.Default) < Permissions.OP && msg.isNotAdmin()
         ) {
             msg.reply("需要权限至少为${Permissions.OP.permissionName}", true)
             return false
